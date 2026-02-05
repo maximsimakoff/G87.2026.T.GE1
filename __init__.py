7a84a01dde4dd20b3f886ed2cb2795e2ec7cabec
Ben <157611695+benhammy96@users.noreply.github.com>
2026-02-05 15:41:46 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from UC3MConsulting.EnterpriseRequest import EnterpriseRequest
-from UC3MConsulting.EnterpriseManager import EnterpriseManager
-from UC3MConsulting.EnterpriseManagementException import EnterpriseManagementException
